5bc7edda45124f3d580980d0ce0af9b5785bfec7
Tarocch1 <huiben.fang@gmail.com>
2020-08-10 16:09:53 +0800

    22. 括号生成

---
+++ problems/22. 括号生成/solution.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  const cache = new Map();
+  /**
+   * @param {number} n
+   * @return {string[]}
+   */
+  const recursion = length => {
+    let result = [];
+    if (length === 0) result.push('');
+    if (cache.has(length)) {
+      return cache.get(length);
+    }
+    for (let i = length; i > 0; i -= 2) {
+      const leftLength = i - 2;
+      const rightLength = length - i;
+      const leftResult = recursion(leftLength);
+      const rightResult = recursion(rightLength);
+      for (let left of leftResult) {
+        for (let right of rightResult) {
+          result.push(`(${left})${right}`);
+        }
+      }
+    }
+    cache.set(length, result);
+    return result;
+  };
+  return recursion(2 * n);
+};
