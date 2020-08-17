2da49af873a6c6c98783d7241c2f8560af93c51f
Tarocch1 <huiben.fang@gmail.com>
2020-08-17 15:14:37 +0800

    56. 合并区间

---
+++ problems/51 - 100/56. 合并区间/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  if (intervals.length === 0) return [];
+  intervals.sort((a, b) => a[0] - b[0]);
+  const result = [];
+  let cur = intervals[0];
+  for (let interval of intervals) {
+    if (interval[0] > cur [1]) {
+      result.push(cur);
+      cur = interval;
+    } else {
+      cur[1] = Math.max(cur[1], interval[1]);
+    }
+  }
+  result.push(cur);
+  return result;
+};
