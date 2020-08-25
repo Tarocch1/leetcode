8951f053a861ac60b220350483861fe4e85863ee
Tarocch1 <huiben.fang@gmail.com>
2020-08-25 10:57:40 +0800

    101. 对称二叉树

---
+++ problems/101 - 150/101. 对称二叉树/solution.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  /**
+   * @param {TreeNode} p
+   * @param {TreeNode} q
+   * @return {boolean}
+   */
+  const check = function (p, q) {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    return p.val === q.val && check(p.left, q.right) && check(p.right, q.left);
+  };
+  return check(root, root);
+};
