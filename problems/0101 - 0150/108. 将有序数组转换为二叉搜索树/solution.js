e211ec51a3d00069c80778eb14e7112c4f05f701
Tarocch1 <huiben.fang@gmail.com>
2020-08-27 10:46:26 +0800

    108. 将有序数组转换为二叉搜索树

---
+++ problems/0101 - 0150/108. 将有序数组转换为二叉搜索树/solution.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  const len = nums.length;
+  if (len === 0) return null;
+  const mid = Math.floor(len / 2);
+  const root = new TreeNode(nums[mid]);
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1, len));
+  return root;
+};
