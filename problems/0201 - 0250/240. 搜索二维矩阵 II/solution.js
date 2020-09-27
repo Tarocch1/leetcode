2370733a893dd3f28e98b807e636a608ab46ecc1
Tarocch1 <huiben.fang@gmail.com>
2020-09-27 16:24:55 +0800

    240. 搜索二维矩阵 II

---
+++ problems/0201 - 0250/240. 搜索二维矩阵 II/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  if (matrix.length === 0 || matrix[0].length === 0) return false;
+  let row = matrix.length - 1,
+    col = 0;
+  while (row >= 0 && col < matrix[0].length) {
+    if (matrix[row][col] > target) {
+      row--;
+    } else if (matrix[row][col] < target) {
+      col++;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
